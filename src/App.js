a9756a5616c5400bda8ac2a5870ea54e1401ef83
joudi awameh <awamehj@gmail.com>
2021-01-18 16:38:22 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+function Header(props){
+  return(
+      <header className= 'App'>
+         <h1>{props.welcome}</h1>
+      </header>
+  )
+}
+
+class Odometer extends React.Component{
+  constructor(){
+
+      super();
+      this.state = {
+          counter : 0,
+      };
+  }
+
+
+  addToOdometer(num){
+    this.state.counter+= num
+    if (this.state.counter >= 9999){
+      this.state.counter -= 9999
+    }
+    
+    this.setState( { 
+      counter: this.state.counter
+    });
+  };
+
+  render(){
+      return(
+          <>
+              <h1 className= 'counter'>{String(this.state.counter).padStart(4,0)}</h1>
+              <div className='main'>
+                <button className= 'counter' onClick={ () => this.addToOdometer(1)}>1</button>
+                <button className= 'counter' onClick={ () => this.addToOdometer(10)}>10</button>
+                <button className= 'counter' onClick={ () => this.addToOdometer(100)}>100</button>
+                <button  className= 'counter'onClick={ () => this.addToOdometer(1000)}>1000</button>
+              </div>
+          </>
+      )
+  }
+
+}
+
+
+
+function Footer(props){
+  return(
+      <header className= 'App'>
+         <h1>{props.trademark}</h1>
+      </header>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Header welcome = "Hello everyone Welcome To My Odometer"/>
+      <Odometer />
+      <Footer trademark = "copy Right @ ASAC"/>
+    </>
   );
 }
 
